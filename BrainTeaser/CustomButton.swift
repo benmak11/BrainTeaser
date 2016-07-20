fe15d6152f80062aba001d793e5130008590aa31
Ben Makusha <btmakusha@gmail.com>
2016-07-20 02:53:21 -0400

    Added code from POP to animate and pulse the button when its clicked

--- BrainTeaser/CustomButton.swift
+++ BrainTeaser/CustomButton.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import pop
 
 @IBDesignable
 class CustomButton: UIButton{
@@ -34,6 +35,33 @@ class CustomButton: UIButton{
     
     func setUpView(){
         self.layer.cornerRadius = cornerRadius
+        
+        self.addTarget(self, action: #selector(CustomButton.scaleToSmall), forControlEvents: .TouchDown)
+        self.addTarget(self, action: #selector(CustomButton.scaleToSmall), forControlEvents: .TouchDragEnter)
+        self.addTarget(self, action: #selector(CustomButton.scaleAnimation), forControlEvents: .TouchUpInside)
+        self.addTarget(self, action: #selector(CustomButton.scaleToDefault), forControlEvents: .TouchDragExit)
+    }
+    
+    func scaleToSmall() {
+        let scaleAnim = POPBasicAnimation(propertyNamed: kPOPLayerScaleXY)
+        scaleAnim.toValue = NSValue(CGSize: CGSizeMake(0.95, 0.95))
+        self.layer.pop_addAnimation(scaleAnim, forKey: "layerScaleSmallAnimation")
+    }
+    
+    func scaleAnimation() {
+        
+        let scaleAnim = POPSpringAnimation(propertyNamed: kPOPLayerScaleXY)
+        scaleAnim.velocity = NSValue(CGSize: CGSizeMake(3.0, 3.0))  //Scales button to desired size
+        scaleAnim.toValue = NSValue(CGSize: CGSizeMake(1.0, 1.0))   //Scales the button back to size
+        scaleAnim.springBounciness = 18
+        self.layer.pop_addAnimation(scaleAnim, forKey: "layerScaleSpringAnimation")
+    }
+    
+    func scaleToDefault() {
+        let scaleAnim = POPBasicAnimation(propertyNamed: kPOPLayerScaleXY)
+        scaleAnim.toValue = NSValue(CGSize: CGSizeMake(1, 1))
+        self.layer.pop_addAnimation(scaleAnim, forKey: "layerScaleDefaultAnimation")
+        
     }
     
 }
