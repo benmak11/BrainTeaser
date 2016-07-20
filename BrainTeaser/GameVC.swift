8084f1051717458cf6a02daeed1214831936a25b
Ben Makusha <btmakusha@gmail.com>
2016-07-20 04:03:44 -0400

    Created XIB file and custom class for the second viewController to alternate through the images provided

---
+++ BrainTeaser/GameVC.swift
@@ -0,0 +1,25 @@
+//
+//  GameVC.swift
+//  BrainTeaser
+//
+//  Created by Baynham Makusha on 7/20/16.
+//  Copyright © 2016 Ben Makusha. All rights reserved.
+//
+
+import UIKit
+
+class GameVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+}
