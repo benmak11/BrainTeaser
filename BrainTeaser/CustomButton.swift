f744a24e1bcd723ea70ca18c16dd2a0baee794e3
Ben Makusha <btmakusha@gmail.com>
2016-07-20 02:34:15 -0400

    Updated the UI for the login screen so it's more user friendly and also created custom classes

---
+++ BrainTeaser/CustomButton.swift
@@ -0,0 +1,39 @@
+//
+//  CustomButton.swift
+//  BrainTeaser
+//
+//  Created by Baynham Makusha on 7/20/16.
+//  Copyright © 2016 Ben Makusha. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class CustomButton: UIButton{
+    
+    @IBInspectable var cornerRadius: CGFloat = 3.0{
+        didSet {
+            setUpView()
+        }
+    }
+    
+    @IBInspectable var fontColor: UIColor = UIColor.whiteColor() {
+        didSet {
+            self.tintColor = fontColor
+        }
+    }
+    
+    override func awakeFromNib() {
+        setUpView()
+    }
+    
+    override func prepareForInterfaceBuilder() {
+        super.prepareForInterfaceBuilder()
+        setUpView()
+    }
+    
+    func setUpView(){
+        self.layer.cornerRadius = cornerRadius
+    }
+    
+}
---
+++ BrainTeaser/CustomTextField.swift
@@ -0,0 +1,44 @@
+//
+//  CustomTextField.swift
+//  BrainTeaser
+//
+//  Created by Baynham Makusha on 7/20/16.
+//  Copyright © 2016 Ben Makusha. All rights reserved.
+//
+
+import UIKit
+
+
+@IBDesignable
+class CustomTextField: UITextField {
+    
+    @IBInspectable var inset: CGFloat = 0
+    
+    @IBInspectable var cornerRadius: CGFloat = 5.0 {
+        didSet {
+            setUpView()
+        }
+    }
+    
+    override func textRectForBounds(bounds: CGRect) -> CGRect {
+        return CGRectInset(bounds, inset, inset)
+    }
+    
+    override func editingRectForBounds(bounds: CGRect) -> CGRect {
+        return textRectForBounds(bounds)
+    }
+    
+    override func awakeFromNib() {
+        self.layer.cornerRadius = 5.0
+    }
+    
+    override func prepareForInterfaceBuilder() {
+        super.prepareForInterfaceBuilder()
+        setUpView()
+        
+    }
+    
+    func setUpView(){
+        self.layer.cornerRadius = cornerRadius
+    }
+}
