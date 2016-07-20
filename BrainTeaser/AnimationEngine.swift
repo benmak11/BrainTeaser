b9a8db96a1534e00f5c88c6ddf62026b7fb07cc5
Ben Makusha <btmakusha@gmail.com>
2016-07-20 03:44:16 -0400

    Added dynamic animation for the Email and Password textFields Also added a friction wait so that the Password textField and Login Button slide in and swing back and forth till centered

---
+++ BrainTeaser/AnimationEngine.swift
@@ -0,0 +1,72 @@
+//
+//  AnimationEngine.swift
+//  BrainTeaser
+//
+//  Created by Baynham Makusha on 7/20/16.
+//  Copyright © 2016 Ben Makusha. All rights reserved.
+//
+
+import UIKit
+import pop
+
+class AnimationEngine {
+    
+    class var offScreenRightPosition: CGPoint {
+        return CGPointMake(UIScreen.mainScreen().bounds.width, CGRectGetMidY(UIScreen.mainScreen().bounds))
+    }
+    
+    class var offScreenLeftPosition: CGPoint {
+        return CGPointMake(-UIScreen.mainScreen().bounds.width, CGRectGetMidY(UIScreen.mainScreen().bounds))
+    }
+    
+    class var screenCenterPosition: CGPoint {
+        return CGPointMake(CGRectGetMidX(UIScreen.mainScreen().bounds), CGRectGetMidX(UIScreen.mainScreen().bounds))
+    }
+    
+    let ANIM_DELAY: Int = 1
+    var originalConstants = [CGFloat]()
+    var constraints: [NSLayoutConstraint]!
+    
+    init(constraints: [NSLayoutConstraint]) {
+        
+        for con in constraints {
+            
+            //Going through all the constraints that were passed from the other screen
+            //Storing the values from the constraints
+            originalConstants.append(con.constant)
+            
+            //Throwing the constraints and they are immediately going to move
+            con.constant = AnimationEngine.offScreenRightPosition.x
+        }
+        
+        self.constraints = constraints
+    }
+    
+    func animateOnScreen(delay: Int) {
+        
+        let time = dispatch_time(DISPATCH_TIME_NOW, Int64(Double(delay) * Double(NSEC_PER_SEC)))
+        
+        dispatch_after(time, dispatch_get_main_queue()) {
+            var index = 0
+            repeat {
+                
+                let moveAnim = POPSpringAnimation(propertyNamed: kPOPLayoutConstraintConstant)
+                moveAnim.toValue = self.originalConstants[index]
+                moveAnim.springBounciness = 12
+                moveAnim.springSpeed = 12
+                
+                if(index > 0){
+                    moveAnim.dynamicsFriction += 10 + CGFloat(index)
+                    moveAnim.dynamicsMass = 5
+                }
+                
+                let con = self.constraints[index]
+                con.pop_addAnimation(moveAnim, forKey: "moveOnScreen")
+                
+                index += 1
+                
+            } while (index < self.constraints.count)
+        }
+        
+    }
+}
